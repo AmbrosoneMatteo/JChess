8d6146808d3b9aa2cf8b9d985628a77f7f46302b
AmbrosoneMatteo <matteo.ambrosone@samtrevano.ch>
2024-03-27 09:49:50 +0100

    Commit Initial

---
+++ JChess/src/jchess/pieces/Bishop.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jchess.pieces;
+
+import jchess.Control;
+
+/**
+ *
+ * @author matteo.ambrosone
+ */
+public class Bishop extends Pieces implements Control {
+
+    @Override
+    public boolean canMove() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ JChess/src/jchess/pieces/Pieces.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jchess.pieces;
+
+/**
+ *
+ * @author matteo.ambrosone
+ */
+public abstract class Pieces {
+    private int x;
+    private int y;
+    private String name;
+    private String side;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSide() {
+        return side;
+    }
+
+    public void setSide(String side) {
+        this.side = side;
+    }
+    
+}
